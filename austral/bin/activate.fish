b1ee7a9cb89bd8f4850a84c527767bd9513cd8a2
Nicolas Iunes <nicolasiuness@gmail.com>
2024-09-23 16:59:57 -0300

    .

--- austral/bin/activate.fish
+++ austral/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nicolas/Desktop/decobike/austral"
+set -gx VIRTUAL_ENV "/Users/nicolas/Desktop/bicicletas/austral"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
